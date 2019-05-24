38f0719093a0d9a3529683d3d9ea919b7b40b86c
Vladimir <mr.qrsant@gmail.com>
2019-05-24 11:16:54 +0300

    правки

--- server.js
+++ server.js
@@ -2,6 +2,55 @@ const http = require('http');
 const Koa = require('koa');
 const app = new Koa();
 
+class SetDayNow {
+  constructor(date) {
+    this.month = date.getMonth() + 1;
+    this.day = date.getDate();
+    this.year = String(date.getFullYear()).slice(2);
+    this.hours = date.getHours();
+    this.min = date.getMinutes();
+    this.sec = date.getSeconds();
+  }
+
+  create() {
+    const dateTime = `${this.check(this.day)}.${this.check(this.month)}.${this.year}`;
+    const time = `${this.check(this.hours)}:${this.check(this.min)}:${this.check(this.sec)}`;
+    return this.final(dateTime, time);
+  }
+
+  check(elem) {
+    if (elem < 10) {
+      return `0${elem}`;
+    }
+    return elem;
+  }
+
+  final(dateTime, time) {
+    return `${dateTime}  ${time}`;
+  }
+}
+
+const dataInfoArr = [];
+
+function MakeOnline(act) {
+  dataInfoArr.push([act, new SetDayNow(new Date()).create()]);
+  return dataInfoArr;
+}
+
+// генерируем событие
+function eventOnline() {
+  const chance = (Math.random() * 100);
+
+  if (chance >= 0 && chance <= 10) { // 10%
+    return 'goal';
+  } else if (chance > 10 && chance <= 50) { // 40%
+    return 'freekick';
+  } else { // 50%
+    return 'action';
+  }
+}
+// генерируем событие
+
 app.use(async (ctx, next) => {
   const origin = ctx.request.get('Origin');
   if (!origin) {
@@ -39,15 +88,27 @@ const router = new Router();
 
 const { streamEvents } = require('http-event-stream');
 const uuid = require('uuid');
+let eventCount = 0;
 
 router.get('/sse', async (ctx) => {
   streamEvents(ctx.req, ctx.res, {
+    async fetch() {
+      return [];
+    },
     stream(sse) {
-      sse.sendEvent({
-        data: new Date().toString(),
-        event: 'date',
-        id: uuid.v4(),
-      });
+      let timerId = setInterval(() => {
+        const onlineEvent = eventOnline(); // чтобы события не сбивались и показывались одинаково
+        sse.sendEvent({
+          data: (MakeOnline(onlineEvent)).toString(), // время публикации поста
+          event: onlineEvent, // не забыть поменять на onlineEvent
+          id: uuid.v4(),
+        });
+        if (eventCount < 50) {
+          eventCount += 1;
+        } else {
+          clearTimeout(timerId); // останавливаем трансляцию
+        }
+      }, 500);
     }
   });
 
@@ -60,6 +121,12 @@ router.get('/index', async (ctx) => {
 
 app.use(router.routes()).use(router.allowedMethods());
 
-const port = process.env.PORT || 7075;
-const server = http.createServer(app.callback()).listen(port)
-server.listen(port);
+// const port = 7075;
+// const server = http.createServer(app.callback()).listen(port)
+// server.listen(port);
+
+const port = 7075;
+http.createServer(app.callback()).listen(port);
+
+// app.use(router.routes()).use(router.allowedMethods());
+// http.createServer(app.callback()).listen(process.env.PORT || 7075);
